c09858281054fe7711a20d85104fd13bf6bdcede
unknown <mitumariam18@gmail.com>
2020-07-13 17:21:08 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+// import uuid from 'uuid';
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4">
+              <TodoInput />
+              <TodoList />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+    render() {
+        return (
+            <div>
+                this is from TODO item component
+            </div>
+        )
+    }
+}
